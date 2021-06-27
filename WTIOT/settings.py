ef167fc2fb3d6db71ccd211de0257e9927e53de8
AmarAgarwal <60135473+AmarAgarwal@users.noreply.github.com>
2021-06-27 16:58:03 +0530

    commit 27/06

--- WTIOT/settings.py
+++ WTIOT/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'WTIOT.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
 
